4eb279d708cdee2a62d022780daff02107847331
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2012-11-01 23:54:11 -0600

    First chapter, practice 1 and 2

---
+++ utils.js
@@ -0,0 +1,8 @@
+var utils = {};
+
+if (!window.requestAnimationFrame) {
+	window.requestAnimationFrame = (window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame
+		|| function (callback){
+			return window.setTimeout(callback, 1000/60);
+		});
+};
